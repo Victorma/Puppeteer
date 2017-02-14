7971b47af73056f5b386398a99fc1aa76fa5e66e
Victormafire <victorma-fire@hotmail.com>
2017-02-14 15:59:41 +0100

    //WIP

---
+++ Assets/IsoUnity/Editor/SecuenceEditor/NodeEditors/NodeEditors/ForkEditors/FormulaForkEditor.cs
@@ -0,0 +1,31 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEditor;
+using NCalc;
+
+[CustomEditor(typeof(FormulaFork))]
+public class FormulaForkEditor : Editor {
+
+    private Expression lastExpression;
+    private object lastValue;
+
+    public override void OnInspectorGUI()
+    {
+        var f = target as FormulaFork;
+
+        EditorGUI.BeginChangeCheck();
+        f.formula = EditorGUILayout.TextField(f.formula);
+
+        if (EditorGUI.EndChangeCheck())
+        {
+            lastExpression = new Expression(f.formula);
+            lastValue = lastExpression.Evaluate();
+        }
+
+        if (lastExpression != null && lastExpression.HasErrors())
+        {
+            EditorGUILayout.LabelField(lastExpression.Error);
+            EditorGUILayout.LabelField(lastValue != null ? lastValue.ToString() : "null");
+        } 
+    }
+}
---
+++ Assets/IsoUnity/Source/Secuences/Forks/FormulaFork.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+using System.Collections;
+using System;
+using NCalc;
+
+public class FormulaFork : Checkable {
+
+    public string formula;
+
+    public override bool check()
+    {
+        Expression ex = new Expression(formula);
+        ex.EvaluateParameter += (param,args) =>
+        {
+            args.HasResult = true;
+            args.Result = IsoSwitchesManager.getInstance().getIsoSwitches().consultSwitch(param);
+        };
+
+        var r = ex.Evaluate();
+        return r is bool ? (bool) r : false;
+    }
+}
