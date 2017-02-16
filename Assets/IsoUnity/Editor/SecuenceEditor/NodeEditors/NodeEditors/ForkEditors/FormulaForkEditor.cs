f67df07d263264d4fe659666705d40c8f0c1781c
Victormafire <victorma-fire@hotmail.com>
2017-02-16 13:35:02 +0100

    Bugs of the last commit fixed

--- Assets/IsoUnity/Editor/SecuenceEditor/NodeEditors/NodeEditors/ForkEditors/FormulaForkEditor.cs
+++ Assets/IsoUnity/Editor/SecuenceEditor/NodeEditors/NodeEditors/ForkEditors/FormulaForkEditor.cs
@@ -5,8 +5,7 @@ using NCalc;
 
 [CustomEditor(typeof(FormulaFork))]
 public class FormulaForkEditor : Editor {
-
-    private Expression lastExpression;
+    
     private object lastValue;
     private List<string> warnings;
 
@@ -22,50 +21,11 @@ public class FormulaForkEditor : Editor {
         var f = target as FormulaFork;
 
         EditorGUI.BeginChangeCheck();
-        f.formula = EditorGUILayout.TextField(f.formula);
+        f.Formula = EditorGUILayout.TextField(f.Formula);
 
-        if (EditorGUI.EndChangeCheck())
+        if (!f.IsValidExpression)
         {
-            warnings.Clear();
-            lastExpression = new Expression(f.formula);
-            lastExpression.EvaluateParameter += LastExpression_EvaluateParameter;
-            lastExpression.EvaluateFunction += LastExpression_EvaluateFunction;
-            lastValue = lastExpression.Evaluate();
-        }
-
-        if (lastExpression != null)
-        {
-            if(lastExpression.HasErrors())
-                EditorGUILayout.LabelField(lastExpression.Error);
-            else if (lastValue != null)
-                EditorGUILayout.LabelField(lastValue != null ? lastValue.ToString() : "null");
-
-            foreach(var w in warnings)
-            {
-                EditorGUILayout.HelpBox(w, MessageType.Warning);
-            }
+            EditorGUILayout.LabelField(f.Error);
         } 
-    }
-
-    private void LastExpression_EvaluateFunction(string name, FunctionArgs args)
-    {
-        throw new System.NotImplementedException();
-    }
-
-    private void LastExpression_EvaluateParameter(string name, ParameterArgs args)
-    {
-
-        var iSwitches = IsoSwitchesManager.getInstance().getIsoSwitches();
-
-        if(iSwitches.containsSwitch(name))
-        {
-            args.HasResult = true;
-            args.Result = iSwitches.consultSwitch(name);
-        }
-        else
-        {
-            args.HasResult = false;
-            warnings.Add("Switch not found: \"" + name +"\"");
-        }
     }
 }
--- Assets/IsoUnity/Source/Secuences/Forks/FormulaFork.cs
+++ Assets/IsoUnity/Source/Secuences/Forks/FormulaFork.cs
@@ -21,10 +21,24 @@ public class FormulaFork : Checkable {
     }
 
     private object expresionResult;
+    private string paramError;
 
     public bool IsValidExpression
     {
-        get { return expression.HasErrors() || !(expresionResult is bool); }
+        get {
+            return !string.IsNullOrEmpty(formula.Trim()) && string.IsNullOrEmpty(paramError) && !expression.HasErrors() && (expresionResult is bool); }
+    }
+
+    public string Error
+    {
+        get { return 
+                string.IsNullOrEmpty(formula.Trim())
+                ? "The formula can't be empty"
+                : !string.IsNullOrEmpty(paramError) 
+                    ? paramError 
+                    : !(expresionResult is bool) 
+                        ? "The formula doesn't result in a boolean value." 
+                        : expression.Error; }
     }
 
     void Awake()
@@ -34,15 +48,31 @@ public class FormulaFork : Checkable {
 
     private void RegenerateExpression()
     {
-        expression = new Expression(this.formula);
-        expression.EvaluateParameter += CheckParameter;
-        expresionResult = expression.Evaluate();
+        paramError = string.Empty;
+        if (!string.IsNullOrEmpty(formula))
+        {
+            try
+            {
+                expression = new Expression(this.formula);
+                expression.EvaluateParameter += CheckParameter;
+                expresionResult = expression.Evaluate();
+            }
+            catch { }
+        }
     }
 
     private void CheckParameter(string param, ParameterArgs args)
     {
-        args.HasResult = true;
-        args.Result = IsoSwitchesManager.getInstance().getIsoSwitches().consultSwitch(param);
+        if (!IsoSwitchesManager.getInstance().getIsoSwitches().containsSwitch(param))
+        {
+            args.HasResult = false;
+            paramError = "Missing parameter \"" + param + "\"";
+        }
+        else
+        {
+            args.HasResult = true;
+            args.Result = IsoSwitchesManager.getInstance().getIsoSwitches().consultSwitch(param);
+        }
     }
 
     public override bool check()
